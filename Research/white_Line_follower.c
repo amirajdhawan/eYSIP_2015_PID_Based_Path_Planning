fb6ea93f2e52b3fba9280279069f6e22a00438b7
Dhirendra Sagar <sagar.dhirendra@gmail.com>
2015-06-11 16:25:06 +0530

    Update white_Line_follower.c

--- Research/white_Line_follower.c
+++ Research/white_Line_follower.c
@@ -1,11 +1,10 @@
-`/************************************************************************************
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
 #include <math.h> //included to support power function
-#include "lcd.c"
+#include "lcd.h"
 
 void port_init();
 void timer5_init();
@@ -67,10 +66,9 @@ void timer5_init()
 	OCR5BL = 0xFF;	//Output compare register low value for Right Motor
 	OCR5CH = 0x00;	//Output compare register high value for Motor C1
 	OCR5CL = 0xFF;	//Output compare register low value for Motor C1
-	TCCR5A = 0xA9;	/*{COM5A1=1, COM5A0=0; COM5B1=1, COM5B0=0; COM5C1=1 COM5C0=0}
- 					  For Overriding normal port functionality to OCRnA outputs.
-				  	  {WGM51=0, WGM50=1} Along With WGM52 in TCCR5B for Selecting FAST PWM 8-bit Mode*/
-	
+	TCCR5A = 0xA9;	//{COM5A1=1, COM5A0=0; COM5B1=1, COM5B0=0; COM5C1=1 COM5C0=0}
+ 			//For Overriding normal port functionality to OCRnA outputs.
+			//{WGM51=0, WGM50=1} Along With WGM52 in TCCR5B for Selecting FAST PWM 8-bit Mode
 	TCCR5B = 0x0B;	//WGM12=1; CS12=0, CS11=1, CS10=1 (Prescaler=64)
 }
 
